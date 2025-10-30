a2a677e9bc5c51531582a876efdb2ad4d5386747
Rachel Horst <95990975+rshorst@users.noreply.github.com>
2025-10-30 16:29:03 +0000

    fix JSX fragment wrapping error

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import FourCsFramework_Integrated from './FourCsFramework_Integrated.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <FourCsFramework_Integrated />
   </StrictMode>,
 )
